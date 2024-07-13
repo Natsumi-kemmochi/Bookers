835f4a27cafa668ee075ba955b35af98e0715073
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-07-13 12:11:06 +0000

    Created table

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,20 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
+   
   end
 
 
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/show'
+    redirect_to '/books'
   end
   
   
   def index
     @books = Book.all
+     @book = Book.new
   end
 
   def show
@@ -27,7 +28,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(list_params)
-    redirect_to book_path(book.id) 
+    redirect_to '/books'
   end
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Rails.application.routes.draw do
   #get 'books/show'
   #get 'books/edit'
   root to: 'homes#top'
-  get 'books/:id' => 'books#show'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
