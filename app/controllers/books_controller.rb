2b0d6f32580936d7d87394c3883b3fec21c5ef82
Natsumi-kemmochi <172781785+Natsumi-kemmochi@users.noreply.github.com>
2024-07-13 14:22:06 +0000

    [finished]Created Edit and Destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,13 +8,13 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
   
   
   def index
     @books = Book.all
-     @book = Book.new
+    @book = Book.new
   end
 
   def show
@@ -27,10 +27,16 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(list_params)
-    redirect_to '/books'
+    book.update(book_params)
+    redirect_to book_path
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    
+  end
   
   private
   
